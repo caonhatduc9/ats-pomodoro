a35cfcfb25ce4bef8f2cdf7a00c452f66f5c5bc7
caonhatduc.cnd <caonhatduc.cnd@gmail.com>
2023-05-04 09:56:58 +0700

    add intercepter fix error cors for frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseProviders } from './database/database.providers';
@@ -8,19 +8,27 @@ import { AuthModule } from './v1/auth/auth.module';
 import { MailingModule } from './v1/mailing/mailing.module';
 import { ConfigModule } from '@nestjs/config';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CorsInterceptor } from './cors.interceptor';
 
 @Module({
+  //fix error cors when frontend fetch API
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    
     DatabaseModule,
     UserModule,
     AuthModule,
     MailingModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CorsInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/cors.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class CorsInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response = context.switchToHttp().getResponse();
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    return next.handle().pipe(
+      tap(() => {
+        // Do nothing on response
+      }),
+    );
+  }
+}
